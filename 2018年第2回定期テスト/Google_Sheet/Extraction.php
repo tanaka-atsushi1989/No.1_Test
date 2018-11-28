be0b2e2a35786c54e04d2aea6d808901f5c6864e
tanaka_atsushi <tanaka.atsushi@no1s.biz>
2018-11-28 19:26:29 +0900

    初回コミット

---
+++ 2018年第2回定期テスト/Google_Sheet/Extraction.php
@@ -0,0 +1,9 @@
+﻿<?php
+$url = "https://sheets.googleapis.com/v4/spreadsheets/11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM/values/sales!A1:E6?fields=values&key=AIzaSyDwxpicDSa3GBcLJmgE1yxdtjYpIJFogcA";
+
+$data = file_get_contents($url);
+
+$data = mb_convert_encoding($data,"sjis","auto");
+
+var_dump($data);
+?>
