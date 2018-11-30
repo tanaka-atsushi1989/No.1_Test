961f20cc8438e37317fbd9b7ebd436a42a138c5f
tanaka_atsushi <tanaka.atsushi@no1s.biz>
2018-11-30 21:02:25 +0900

    完成コミット

--- 2018年第2回定期テスト/Google_Sheet/Extraction.php
+++ 2018年第2回定期テスト/Google_Sheet/Extraction.php
@@ -1,9 +1,25 @@
 ﻿<?php
-$url = "https://sheets.googleapis.com/v4/spreadsheets/11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM/values/sales!A1:E6?fields=values&key=AIzaSyDwxpicDSa3GBcLJmgE1yxdtjYpIJFogcA";
+$url = "https://sheets.googleapis.com/v4/spreadsheets/11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM/values/sales!A1:E6?fields=values&key=";
 
 $data = file_get_contents($url);
 
-$data = mb_convert_encoding($data,"sjis","auto");
+$data = json_decode($data);
+$i = 0;
 
-var_dump($data);
+foreach ($data->values as $values) {
+    foreach ($values as $value) {
+    	
+		$value = mb_convert_encoding($value,"sjis-win","auto");
+		echo "'";
+		echo $value;
+		echo "'";
+		echo ",";
+		
+		$i++;
+		if($i % 5 == 0) {
+			echo "\n";
+		}
+	}
+}
+
 ?>
